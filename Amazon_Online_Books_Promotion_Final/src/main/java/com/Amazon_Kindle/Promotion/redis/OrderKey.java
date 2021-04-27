aa67ff8f84505392babc9227ff20dca7a23b2d50
Kentwwx <38131941+Kentwwx@users.noreply.github.com>
2021-04-27 10:02:33 +0800

    Delete Amazon_Online_Books_Promotion_Final directory

--- Amazon_Online_Books_Promotion_Final/src/main/java/com/Amazon_Kindle/Promotion/redis/OrderKey.java
+++
@@ -1,9 +0,0 @@
-package com.Amazon_Kindle.Promotion.redis;
-
-public class OrderKey extends BasePrefix {
-
-	public OrderKey(String prefix) {
-		super(prefix);
-	}
-	public static OrderKey getMiaoshaOrderByUidGid = new OrderKey("moug");
-}
